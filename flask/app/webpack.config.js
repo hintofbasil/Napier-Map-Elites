f5268760458393854bd2b9091e511be4c4450673
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2018-06-05 10:01:07 +0100

    Added processing of multiple files

--- flask/app/webpack.config.js
+++ flask/app/webpack.config.js
@@ -1,10 +1,17 @@
+const glob = require('glob');
 const path = require('path');
+
+var files = glob.sync('./js/*.js').reduce(function(result, item) {
+  var name = path.basename(item, path.extname(item));
+  result[name] = item;
+  return result;
+}, {});
 
 module.exports = {
   mode: 'development',
-  entry: './js/index.js',
+  entry: files,
   output: {
-    filename: 'bundle.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'static/js')
   }
 };
