f655bb577ac97436ef51e4911df3be41dea72233
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2018-06-06 09:56:24 +0100

    Added processing of sass files

--- flask/app/webpack.config.js
+++ flask/app/webpack.config.js
@@ -1,17 +1,44 @@
 const glob = require('glob');
 const path = require('path');
 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const devMode = process.env.NODE_ENV !== 'production'
+
 var files = glob.sync('./js/*.js').reduce(function(result, item) {
-  var name = path.basename(item, path.extname(item));
-  result[name] = item;
+  var name = path.basename(item);
+  result['js/' + name] = item;
   return result;
 }, {});
+
+var files = glob.sync('./sass/*.scss').reduce(function(result, item) {
+  var name = path.basename(item);
+  result['css/' + name] = item;
+  return result;
+}, files);
 
 module.exports = {
   mode: 'development',
   entry: files,
   output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'static/js')
-  }
+    filename: '[name]',
+    path: path.resolve(__dirname, 'static')
+  },
+
+  // Sass
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ]
 };
