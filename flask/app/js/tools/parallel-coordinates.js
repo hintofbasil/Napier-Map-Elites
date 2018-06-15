a60aac89f0a37788e025713644372e96a6f368a6
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2018-06-15 14:25:37 +0100

    Updated parcoords to use point based brushing

--- flask/app/js/tools/parallel-coordinates.js
+++ flask/app/js/tools/parallel-coordinates.js
@@ -3,12 +3,12 @@ class ParallelCoordinates {
   constructor(elementId, data, onFiltered) {
     this.data = data;
     this.onFiltered = onFiltered;
-    var graph = d3.parcoords()(elementId)
+    var graph = ParCoords()(elementId)
       .data(this.get_data())
       .dimensions(this.get_dimensions())
       .render()
       .createAxes()
-      .brushMode('1D-axes');
+      .brushMode('1D-points');
     graph.on('brush', (filtered) => {
       var elements = filtered.map((l) => { return this.data.data[l.join(':')]; } );
       this.onFiltered(elements);
