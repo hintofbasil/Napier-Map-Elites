59ec606f9241453b7cb5a690aa07d263721ad79f
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2018-06-18 14:58:32 +0100

    Added colouring to parallel coordinates graph

--- flask/app/js/tools/parallel-coordinates.js
+++ flask/app/js/tools/parallel-coordinates.js
@@ -1,9 +1,28 @@
+require('d3-scale');
+
 class ParallelCoordinates {
 
   constructor(elementId, data, onFiltered) {
+
+    var scale = d3.scaleLinear()
+        .domain([data.distanceRange[1], data.distanceRange[0]])
+        .range([0,1]);
+
+    var color = d => {
+      //var value = 0.4;
+      var key = d.join(':');
+      var distance = this.data.data[key].distance[1];
+      var value = scale(distance);
+
+      //value from 0 to 1
+      var hue=((1-value)*120).toString(10);
+      return ["hsl(",hue,",100%,50%)"].join("");
+    }
+
     this.data = data;
     this.onFiltered = onFiltered;
     var graph = ParCoords()(elementId)
+      .color(color)
       .data(this.get_data())
       .dimensions(this.get_dimensions())
       .render()
