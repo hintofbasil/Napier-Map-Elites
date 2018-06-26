f9afa291a368afee04962bd13364de94ba4ce198
hintofbasil <CuteCondemnedPenguin@shadowmail.co.uk>
2018-06-26 16:11:57 +0100

    Added rendering of markdown for a single solution

--- flask/app/views/solutions.py
+++ flask/app/views/solutions.py
@@ -1,7 +1,9 @@
 import os
+from zipfile import ZipFile
 
-from flask import jsonify
+from flask import abort, jsonify, render_template
 from flask_api import status
+import markdown
 
 from main import app
 
@@ -23,3 +25,21 @@ def solution_exists(solution_hash):
             status='Err',
         )
     ), status.HTTP_404_NOT_FOUND
+
+@app.route('/solution/<solution_hash>/<solution_key>', methods=['GET'])
+def get_solution(solution_hash, solution_key):
+    path = os.path.join(
+        app.config['SOLUTION_UPLOAD_FOLDER'], solution_hash + '.zip'
+    )
+    if not os.path.exists(path):
+        abort(404)
+    with ZipFile(path, 'r') as z:
+        files = [x for x in z.namelist() if x.startswith(solution_key)]
+        if not files:
+            abort(404)
+        markdownFile = [x for x in files if x.endswith('.md')]
+        html = ''
+        if markdownFile:
+            with z.open(markdownFile[0], 'r') as mf:
+                html = markdown.markdown(mf.read().decode('utf-8'))
+                return render_template('base.html', content=html)
